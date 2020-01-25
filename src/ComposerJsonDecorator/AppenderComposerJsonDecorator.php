50e7120119cb1e7ae38f7e80b539f4d1f21232be
TomasVotruba <tomas.vot@gmail.com>
2020-01-25 11:12:09 +0100

    spacing

---
+++ src/ComposerJsonDecorator/AppenderComposerJsonDecorator.php
@@ -0,0 +1,55 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Symplify\MonorepoBuilder\ComposerJsonDecorator;
+
+use Symplify\MonorepoBuilder\Contract\ComposerJsonDecoratorInterface;
+use Symplify\PackageBuilder\Yaml\ParametersMerger;
+
+final class AppenderComposerJsonDecorator implements ComposerJsonDecoratorInterface
+{
+    /**
+     * @var mixed[]
+     */
+    private $dataToAppend = [];
+
+    /**
+     * @var ParametersMerger
+     */
+    private $parametersMerger;
+
+    /**
+     * @param mixed[] $dataToAppend
+     */
+    public function __construct(array $dataToAppend, ParametersMerger $parametersMerger)
+    {
+        $this->dataToAppend = $dataToAppend;
+        $this->parametersMerger = $parametersMerger;
+    }
+
+    /**
+     * @param mixed[] $composerJson
+     * @return mixed[]
+     */
+    public function decorate(array $composerJson): array
+    {
+        foreach (array_keys($composerJson) as $key) {
+            if (! isset($this->dataToAppend[$key])) {
+                continue;
+            }
+
+            $composerJson[$key] = $this->parametersMerger->merge($this->dataToAppend[$key], $composerJson[$key]);
+
+            unset($this->dataToAppend[$key]);
+
+            // fix unique repositories
+            if ($key === 'repositories') {
+                $composerJson[$key] = array_unique($composerJson[$key], SORT_REGULAR);
+            }
+        }
+
+        // add what was skipped
+        return array_merge($composerJson, $this->dataToAppend);
+    }
+}
---
+++ tests/ComposerJsonDecorator/SortRequireComposerJsonDecoratorTest.php
@@ -0,0 +1,62 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Symplify\MonorepoBuilder\Tests\ComposerJsonDecorator;
+
+use Symplify\MonorepoBuilder\ComposerJsonDecorator\SortRequireComposerJsonDecorator;
+use Symplify\MonorepoBuilder\HttpKernel\MonorepoBuilderKernel;
+use Symplify\PackageBuilder\Tests\AbstractKernelTestCase;
+
+final class SortRequireComposerJsonDecoratorTest extends AbstractKernelTestCase
+{
+    /**
+     * @var mixed[]
+     */
+    private $composerJson = [
+        'required' => [
+            'b' => 'v1.0.0',
+            'a' => 'v1.0.0',
+        ],
+    ];
+
+    /**
+     * @var SortRequireComposerJsonDecorator
+     */
+    private $sortRequireComposerJsonDecorator;
+
+    protected function setUp(): void
+    {
+        $this->bootKernel(MonorepoBuilderKernel::class);
+
+        $this->sortRequireComposerJsonDecorator = self::$container->get(SortRequireComposerJsonDecorator::class);
+    }
+
+    public function testNoSort(): void
+    {
+        $decorated = $this->sortRequireComposerJsonDecorator->decorate($this->composerJson);
+
+        $this->assertSame($this->composerJson, $decorated);
+    }
+
+    public function testSort(): void
+    {
+        $composerJsonWithSort = $this->composerJson + [
+            'config' => [
+                'sort-packages' => true,
+            ],
+        ];
+
+        $decorated = $this->sortRequireComposerJsonDecorator->decorate($composerJsonWithSort);
+
+        $this->assertSame([
+            'required' => [
+                'b' => 'v1.0.0',
+                'a' => 'v1.0.0',
+            ],
+            'config' => [
+                'sort-packages' => true,
+            ],
+        ], $decorated);
+    }
+}
