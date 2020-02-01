e9978f36463e207cb2cbafe2f95a28bfe3aed5c3
natepage <nathan.page86@gmail.com>
2020-02-01 11:52:03 +1100

    [MonorepoBuilder][WIP] Fix ecs and phpstan

---
+++ packages/merge/src/ComposerKeyMerger/RequireDevComposerKeyMerger.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Symplify\MonorepoBuilder\Merge\ComposerKeyMerger;
+
+use Symplify\MonorepoBuilder\ComposerJsonObject\ValueObject\ComposerJson;
+use Symplify\MonorepoBuilder\Merge\Contract\ComposerKeyMergerInterface;
+
+final class RequireDevComposerKeyMerger extends AbstractComposerKeyMerger implements ComposerKeyMergerInterface
+{
+    public function merge(ComposerJson $mainComposerJson, ComposerJson $newComposerJson): void
+    {
+        if ($newComposerJson->getRequireDev() === []) {
+            return;
+        }
+
+        $requireDev = $this->mergeRecursiveAndSort(
+            $mainComposerJson->getRequireDev(),
+            $newComposerJson->getRequireDev()
+        );
+        $mainComposerJson->setRequireDev($requireDev);
+    }
+}
