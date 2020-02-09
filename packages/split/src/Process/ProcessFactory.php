3ec38625ded3324f5c8f9e32279e23f824294721
natepage <nathan.page86@gmail.com>
2020-02-09 17:45:37 +1100

    [MonorepoBuilder] Fix ecs

---
+++ packages/split/src/Process/ProcessFactory.php
@@ -0,0 +1,76 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Symplify\MonorepoBuilder\Split\Process;
+
+use Nette\Utils\FileSystem;
+use Nette\Utils\Strings;
+use Symfony\Component\Process\Process;
+use Symplify\MonorepoBuilder\Split\Configuration\RepositoryGuard;
+
+final class ProcessFactory
+{
+    /**
+     * @var string
+     */
+    private const SUBSPLIT_BASH_FILE = __DIR__ . '/../../bash/subsplit.sh';
+
+    /**
+     * @var string
+     */
+    private $repository;
+
+    /**
+     * @var string
+     */
+    private $subsplitCacheDirectory;
+
+    /**
+     * @var RepositoryGuard
+     */
+    private $repositoryGuard;
+
+    public function __construct(
+        RepositoryGuard $repositoryGuard,
+        string $subsplitCacheDirectory,
+        string $repository
+    ) {
+        $this->repositoryGuard = $repositoryGuard;
+        $this->subsplitCacheDirectory = $subsplitCacheDirectory;
+        $this->repository = $repository;
+    }
+
+    public function createSubsplit(
+        string $branch,
+        ?string $theMostRecentTag,
+        string $directory,
+        string $remoteRepository
+    ): Process {
+        $this->repositoryGuard->ensureIsRepository($remoteRepository);
+
+        $commandLine = [
+            realpath(self::SUBSPLIT_BASH_FILE),
+            sprintf('--from-directory=%s', $directory),
+            sprintf('--to-repository=%s', $remoteRepository),
+            sprintf('--branch=%s', $branch),
+            $theMostRecentTag ? sprintf('--tag=%s', $theMostRecentTag) : '',
+            sprintf('--repository=%s', $this->repository),
+        ];
+
+        return $this->createProcessFromCommandLine($commandLine, $directory);
+    }
+
+    /**
+     * @param mixed[] $commandLine
+     */
+    private function createProcessFromCommandLine(array $commandLine, string $directory): Process
+    {
+        $directory = $this->subsplitCacheDirectory . DIRECTORY_SEPARATOR . Strings::webalize($directory);
+
+        FileSystem::delete($directory);
+        FileSystem::createDir($directory);
+
+        return new Process($commandLine, $directory, null, null, null);
+    }
+}
