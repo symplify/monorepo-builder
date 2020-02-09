ae578222fdad8b1ba1da46fb8fa983e116b3d489
TomasVotruba <tomas.vot@gmail.com>
2020-02-09 18:28:38 +0100

    use fixed phpstan rector

---
+++ packages/split/tests/Process/ProcessFactoryTest.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Symplify\MonorepoBuilder\Split\Tests\Process;
+
+use Symplify\MonorepoBuilder\HttpKernel\MonorepoBuilderKernel;
+use Symplify\MonorepoBuilder\Split\Process\ProcessFactory;
+use Symplify\PackageBuilder\Tests\AbstractKernelTestCase;
+
+final class ProcessFactoryTest extends AbstractKernelTestCase
+{
+    /**
+     * @var ProcessFactory
+     */
+    private $processFactory;
+
+    protected function setUp(): void
+    {
+        $this->bootKernel(MonorepoBuilderKernel::class);
+
+        $this->processFactory = self::$container->get(ProcessFactory::class);
+    }
+
+    public function test(): void
+    {
+        $subsplitProcess = $this->processFactory->createSubsplit(
+            'master',
+            '',
+            'localDirectory',
+            'git@github.com:Symplify/Symplify.git'
+        );
+
+        $subsplitRealpath = realpath(__DIR__ . '/../../bash/subsplit.sh');
+        $commandLine = "'" . $subsplitRealpath . "' '--from-directory=localDirectory' '--to-repository=git@github.com:Symplify/Symplify.git' '--branch=master' %s '--repository=%s/.git'";
+        $this->assertStringMatchesFormat($commandLine, $subsplitProcess->getCommandLine());
+    }
+}
