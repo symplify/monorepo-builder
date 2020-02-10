9278bc0d16c6880915eda1548eb64b27c75183a7
TomasVotruba <tomas.vot@gmail.com>
2020-02-10 14:07:36 +0100

    add README check

---
+++ packages/release/src/Message.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Symplify\MonorepoBuilder\Release;
+
+final class Message
+{
+    /**
+     * @var string
+     */
+    public const SUCCESS = 'All good!';
+}
