d21665d25c0f603c86a423577f1bd053133f8ab6
Tomas Votruba <tomas.vot@gmail.com>
2020-02-13 11:58:26 +0100

    Merge pull request #1782 from vrbata/FLEXLOADER_SERVICE_ORDER
    [FlexLoader] Priority of loaded configs

---
+++ tests/FileSystem/JsonFileManager/JsonFileManagerTest.php
@@ -0,0 +1,68 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Symplify\MonorepoBuilder\Tests\FileSystem\JsonFileManager;
+
+use Symfony\Component\Filesystem\Filesystem;
+use Symplify\MonorepoBuilder\FileSystem\JsonFileManager;
+use Symplify\MonorepoBuilder\HttpKernel\MonorepoBuilderKernel;
+use Symplify\PackageBuilder\Tests\AbstractKernelTestCase;
+use Symplify\SmartFileSystem\SmartFileInfo;
+
+final class JsonFileManagerTest extends AbstractKernelTestCase
+{
+    /**
+     * @var JsonFileManager
+     */
+    private $jsonFileManager;
+
+    /**
+     * @var Filesystem
+     */
+    private $filesystem;
+
+    protected function setUp(): void
+    {
+        $this->bootKernel(MonorepoBuilderKernel::class);
+
+        $this->jsonFileManager = self::$container->get(JsonFileManager::class);
+        $this->filesystem = self::$container->get(Filesystem::class);
+    }
+
+    protected function tearDown(): void
+    {
+        $this->filesystem->remove(__DIR__ . '/Source/second.json');
+        $this->filesystem->remove(__DIR__ . '/Source/third.json');
+    }
+
+    public function testLoad(): void
+    {
+        $expectedJson = [
+            'key' => 'value',
+        ];
+
+        $this->assertSame($expectedJson, $this->jsonFileManager->loadFromFilePath(__DIR__ . '/Source/first.json'));
+
+        $this->assertSame(
+            $expectedJson,
+            $this->jsonFileManager->loadFromFileInfo(new SmartFileInfo(__DIR__ . '/Source/first.json'))
+        );
+    }
+
+    public function testEncodeArrayToString(): void
+    {
+        $jsonContent = $this->jsonFileManager->encodeJsonToFileContent(['another_key' => 'another_value']);
+        $this->assertStringEqualsFile(__DIR__ . '/Source/expected-second.json', $jsonContent);
+    }
+
+    public function testSaveWithInlinedSections(): void
+    {
+        $fileContent = $this->jsonFileManager->encodeJsonToFileContent([
+            'inline_section' => [1, 2, 3],
+            'normal_section' => [1, 2, 3],
+        ], ['inline_section']);
+
+        $this->assertStringEqualsFile(__DIR__ . '/Source/expected-inlined.json', $fileContent);
+    }
+}
