6f4787945b17491d7809ee9332fd70ce10a7293e
TomasVotruba <tomas.vot@gmail.com>
2020-03-12 09:53:36 +0100

    [SymfonyStaticDumper] Add new package

---
+++ packages/split/src/Configuration/Option.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Symplify\MonorepoBuilder\Split\Configuration;
+
+final class Option
+{
+    /**
+     * @var string
+     */
+    public const MAX_PROCESSES = 'max-processes';
+
+    /**
+     * @var string
+     */
+    public const TAG = 'tag';
+}
