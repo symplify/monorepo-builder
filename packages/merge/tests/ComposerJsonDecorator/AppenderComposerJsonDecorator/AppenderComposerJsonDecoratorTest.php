ee5c928ecaf19d03a3bfb84cba0d21e68a0af7fd
Tomas Votruba <tomas.vot@gmail.com>
2020-03-19 00:09:37 +0100

    Merge pull request #1816 from cgaube/ISS_1814
    ISS_1814 Fixing the merge command of the monorepo builder tool

---
+++ packages/merge/tests/ComposerJsonDecorator/AppenderComposerJsonDecorator/AppenderComposerJsonDecoratorTest.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Symplify\MonorepoBuilder\Merge\Tests\ComposerJsonDecorator\AppenderComposerJsonDecorator;
+
+use Symplify\MonorepoBuilder\ComposerJsonObject\ComposerJsonFactory;
+use Symplify\MonorepoBuilder\HttpKernel\MonorepoBuilderKernel;
+use Symplify\MonorepoBuilder\Merge\ComposerJsonDecorator\AppenderComposerJsonDecorator;
+use Symplify\MonorepoBuilder\Merge\Tests\ComposerJsonDecorator\AbstractComposerJsonDecoratorTest;
+
+final class AppenderComposerJsonDecoratorTest extends AbstractComposerJsonDecoratorTest
+{
+    /**
+     * @var AppenderComposerJsonDecorator
+     */
+    private $appenderComposerJsonDecorator;
+
+    protected function setUp(): void
+    {
+        $this->bootKernelWithConfigs(MonorepoBuilderKernel::class, [__DIR__ . '/Source/appending_config.yaml']);
+
+        $this->composerJsonFactory = self::$container->get(ComposerJsonFactory::class);
+        $this->appenderComposerJsonDecorator = self::$container->get(AppenderComposerJsonDecorator::class);
+    }
+
+    public function test(): void
+    {
+        $composerJson = $this->createComposerJson(__DIR__ . '/Source/input.json');
+        $this->appenderComposerJsonDecorator->decorate($composerJson);
+
+        $this->assertComposerJsonEquals(__DIR__ . '/Source/expected.json', $composerJson);
+    }
+}
