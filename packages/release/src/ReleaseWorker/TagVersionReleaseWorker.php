c0f25a89c0f2b7b1b39035247ddb70cf783d427c
TomasVotruba <tomas.vot@gmail.com>
2020-04-08 18:16:09 +0200

    prepare release

---
+++ packages/release/src/ReleaseWorker/TagVersionReleaseWorker.php
@@ -0,0 +1,44 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Symplify\MonorepoBuilder\Release\ReleaseWorker;
+
+use PharIo\Version\Version;
+use Symplify\MonorepoBuilder\Release\Contract\ReleaseWorker\ReleaseWorkerInterface;
+use Symplify\MonorepoBuilder\Release\Process\ProcessRunner;
+use Throwable;
+
+final class TagVersionReleaseWorker implements ReleaseWorkerInterface
+{
+    /**
+     * @var ProcessRunner
+     */
+    private $processRunner;
+
+    public function __construct(ProcessRunner $processRunner)
+    {
+        $this->processRunner = $processRunner;
+    }
+
+    public function getPriority(): int
+    {
+        return 400;
+    }
+
+    public function work(Version $version): void
+    {
+        try {
+            $this->processRunner->run('git add . && git commit -m "prepare release" && git push origin master');
+        } catch (Throwable $throwable) {
+            // nothing to commit
+        }
+
+        $this->processRunner->run('git tag ' . $version->getVersionString());
+    }
+
+    public function getDescription(Version $version): string
+    {
+        return sprintf('Add local tag "%s"', $version->getVersionString());
+    }
+}
