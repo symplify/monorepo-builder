5bf561917d600f013afeb431b86795ec180def61
TomasVotruba <tomas.vot@gmail.com>
2020-04-10 01:55:34 +0200

    prepare release

---
+++ packages/split/src/Configuration/RepositoryGuard.php
@@ -0,0 +1,54 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Symplify\MonorepoBuilder\Split\Configuration;
+
+use Nette\Utils\Strings;
+use Symplify\MonorepoBuilder\Split\Exception\InvalidGitRepositoryException;
+use Symplify\MonorepoBuilder\Split\Exception\InvalidRepositoryFormatException;
+use Symplify\SmartFileSystem\FileSystemGuard;
+
+final class RepositoryGuard
+{
+    /**
+     * @var string
+     */
+    private const GIT_REPOSITORY_PATTERN = '#((git|ssh|http(s)?|file)|(git@[\w\.]+)|[\w]+)(:(//)?)([\w\.@\:/\-~]+)(\.git)?(/)?#';
+
+    /**
+     * @var FileSystemGuard
+     */
+    private $fileSystemGuard;
+
+    public function __construct(FileSystemGuard $fileSystemGuard)
+    {
+        $this->fileSystemGuard = $fileSystemGuard;
+    }
+
+    public function ensureIsRepository(string $possibleRepository): void
+    {
+        // local repositorye
+        if ($possibleRepository === '.git') {
+            return;
+        }
+
+        if (Strings::match($possibleRepository, self::GIT_REPOSITORY_PATTERN)) {
+            return;
+        }
+
+        throw new InvalidRepositoryFormatException(sprintf('"%s" is not format for repository', $possibleRepository));
+    }
+
+    public function ensureIsRepositoryDirectory(string $repositoryDirectory): void
+    {
+        $this->fileSystemGuard->ensureDirectoryExists($repositoryDirectory);
+
+        if (! file_exists($repositoryDirectory . '/.git')) {
+            throw new InvalidGitRepositoryException(sprintf(
+                '.git was not found in "%s" directory',
+                $repositoryDirectory
+            ));
+        }
+    }
+}
