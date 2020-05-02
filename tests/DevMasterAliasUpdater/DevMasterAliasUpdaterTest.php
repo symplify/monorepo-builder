2804c4afb9d474f4e2834f1293c69ac359ac4417
TomasVotruba <tomas.vot@gmail.com>
2020-05-02 22:49:44 +0200

    [CS] Remove unused MethodWrapper

---
+++ tests/DevMasterAliasUpdater/DevMasterAliasUpdaterTest.php
@@ -0,0 +1,40 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Symplify\MonorepoBuilder\Tests\DevMasterAliasUpdater;
+
+use Nette\Utils\FileSystem;
+use Symplify\MonorepoBuilder\DevMasterAliasUpdater;
+use Symplify\MonorepoBuilder\HttpKernel\MonorepoBuilderKernel;
+use Symplify\PackageBuilder\Tests\AbstractKernelTestCase;
+use Symplify\SmartFileSystem\SmartFileInfo;
+
+final class DevMasterAliasUpdaterTest extends AbstractKernelTestCase
+{
+    /**
+     * @var DevMasterAliasUpdater
+     */
+    private $devMasterAliasUpdater;
+
+    protected function setUp(): void
+    {
+        $this->bootKernel(MonorepoBuilderKernel::class);
+
+        $this->devMasterAliasUpdater = self::$container->get(DevMasterAliasUpdater::class);
+    }
+
+    protected function tearDown(): void
+    {
+        FileSystem::copy(__DIR__ . '/Source/backup-first.json', __DIR__ . '/Source/first.json');
+    }
+
+    public function test(): void
+    {
+        $fileInfos = [new SmartFileInfo(__DIR__ . '/Source/first.json')];
+
+        $this->devMasterAliasUpdater->updateFileInfosWithAlias($fileInfos, '4.5-dev');
+
+        $this->assertFileEquals(__DIR__ . '/Source/expected-first.json', __DIR__ . '/Source/first.json');
+    }
+}
