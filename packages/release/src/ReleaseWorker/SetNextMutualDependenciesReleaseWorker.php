17d86a31cc315dd270e5d5b2de7baa54f2a27637
TomasVotruba <tomas.vot@gmail.com>
2020-05-04 12:14:51 +0200

    open 7.4-dev

---
+++ packages/release/src/ReleaseWorker/SetNextMutualDependenciesReleaseWorker.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Symplify\MonorepoBuilder\Release\ReleaseWorker;
+
+use PharIo\Version\Version;
+
+final class SetNextMutualDependenciesReleaseWorker extends AbstractMutualDependencyReleaseWorker
+{
+    public function getPriority(): int
+    {
+        return 200;
+    }
+
+    public function work(Version $version): void
+    {
+        $versionInString = $this->versionUtils->getRequiredNextFormat($version);
+
+        $this->dependencyUpdater->updateFileInfosWithPackagesAndVersion(
+            $this->composerJsonProvider->getPackagesFileInfos(),
+            $this->packageNamesProvider->provide(),
+            $versionInString
+        );
+    }
+
+    public function getDescription(Version $version): string
+    {
+        $versionInString = $this->versionUtils->getRequiredNextFormat($version);
+
+        return sprintf('Set packages mutual dependencies to "%s" (alias of dev version)', $versionInString);
+    }
+}
