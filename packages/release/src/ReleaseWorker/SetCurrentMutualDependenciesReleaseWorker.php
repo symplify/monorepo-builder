23dd6b90bd81f2a3ea410252b4e5d0554a9b258a
TomasVotruba <tomas.vot@gmail.com>
2020-05-07 00:42:24 +0200

    [ECS] Change AppliedCheckerCollector to use real path over objects as key

---
+++ packages/release/src/ReleaseWorker/SetCurrentMutualDependenciesReleaseWorker.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Symplify\MonorepoBuilder\Release\ReleaseWorker;
+
+use PharIo\Version\Version;
+
+final class SetCurrentMutualDependenciesReleaseWorker extends AbstractMutualDependencyReleaseWorker
+{
+    public function getPriority(): int
+    {
+        return 800;
+    }
+
+    public function work(Version $version): void
+    {
+        $versionInString = $this->versionUtils->getRequiredFormat($version);
+
+        $this->dependencyUpdater->updateFileInfosWithPackagesAndVersion(
+            $this->composerJsonProvider->getPackagesFileInfos(),
+            $this->packageNamesProvider->provide(),
+            $versionInString
+        );
+    }
+
+    public function getDescription(Version $version): string
+    {
+        $versionInString = $this->versionUtils->getRequiredFormat($version);
+
+        return sprintf('Set packages mutual dependencies to "%s" version', $versionInString);
+    }
+}
