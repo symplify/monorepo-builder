00da629b3c51e4bae4d338794566c858ae83f420
TomasVotruba <tomas.vot@gmail.com>
2020-07-04 12:55:27 +0200

    [CodingStanard] Add NoIssetOrEmptyOnObjectRule

---
+++ src/Console/Reporter/ConflictingPackageVersionsReporter.php
@@ -0,0 +1,47 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Symplify\MonorepoBuilder\Console\Reporter;
+
+use Symfony\Component\Console\Style\SymfonyStyle;
+use Symplify\SmartFileSystem\SmartFileInfo;
+
+final class ConflictingPackageVersionsReporter
+{
+    /**
+     * @var SymfonyStyle
+     */
+    private $symfonyStyle;
+
+    public function __construct(SymfonyStyle $symfonyStyle)
+    {
+        $this->symfonyStyle = $symfonyStyle;
+    }
+
+    /**
+     * @param mixed[] $conflictingPackages
+     */
+    public function report(array $conflictingPackages): void
+    {
+        foreach ($conflictingPackages as $packageName => $filesToVersions) {
+            $this->symfonyStyle->title(sprintf('Package "%s" has incompatible version', $packageName));
+
+            $tableRows = $this->createTableRows($filesToVersions);
+            $this->symfonyStyle->table(['File', 'Version'], $tableRows);
+        }
+
+        $this->symfonyStyle->error('Found conflicting package versions, fix them first.');
+    }
+
+    private function createTableRows($filesToVersions): array
+    {
+        $tableRows = [];
+
+        foreach ($filesToVersions as $file => $version) {
+            $fileInfo = new SmartFileInfo($file);
+            $tableRows[] = [$fileInfo->getRelativeFilePathFromCwd(), $version];
+        }
+        return $tableRows;
+    }
+}
