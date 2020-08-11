f401a038711d12183a257cf16b7c5c5155c0f44b
Tomas Votruba <tomas.vot@gmail.com>
2020-08-11 23:30:58 +0200

    Merge pull request #2051 from seb-jean/patch-3
    Update php-cs-fixer-psr2.php

---
+++ packages/release/config/config.php
@@ -0,0 +1,20 @@
+<?php declare(strict_types=1);
+
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+
+return static function (ContainerConfigurator $containerConfigurator): void {
+    $parameters = $containerConfigurator->parameters();
+
+    $parameters->set('is_stage_required', false);
+
+    $parameters->set('stages_to_allow_existing_tag', []);
+
+    $services = $containerConfigurator->services();
+
+    $services->defaults()
+        ->autowire()
+        ->public();
+
+    $services->load('Symplify\MonorepoBuilder\Release\\', __DIR__ . '/../src')
+        ->exclude([__DIR__ . '/../src/Exception/*', __DIR__ . '/../src/ReleaseWorker/*']);
+};
