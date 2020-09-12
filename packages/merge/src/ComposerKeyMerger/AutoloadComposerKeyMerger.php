73cb1816cbf0efe92876bd77169cdd955cb61827
Tomas Votruba <tomas.vot@gmail.com>
2020-09-13 00:32:08 +0200

    [SmartFileSystem] Add SmartFinder (#2162)
    * [SmartFileSystem] Add SmartFinder
    
    * [rector] [SmartFileSystem] Add SmartFinder
    
    Co-authored-by: rector-bot <tomas@getrector.org>

---
+++ packages/merge/src/ComposerKeyMerger/AutoloadComposerKeyMerger.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Symplify\MonorepoBuilder\Merge\ComposerKeyMerger;
+
+use Symplify\ComposerJsonManipulator\ValueObject\ComposerJson;
+use Symplify\MonorepoBuilder\Merge\Contract\ComposerKeyMergerInterface;
+
+final class AutoloadComposerKeyMerger extends AbstractComposerKeyMerger implements ComposerKeyMergerInterface
+{
+    public function merge(ComposerJson $mainComposerJson, ComposerJson $newComposerJson): void
+    {
+        if ($newComposerJson->getAutoload() === []) {
+            return;
+        }
+
+        $autoload = $this->mergeRecursiveAndSort($mainComposerJson->getAutoload(), $newComposerJson->getAutoload());
+        $mainComposerJson->setAutoload($autoload);
+    }
+}
