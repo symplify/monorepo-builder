e01a6bfc22f3f951f3fb95230fcd26fc35ebaf0e
GitHub Action <action@github.com>
2021-07-03 10:44:12 +0000

    Updated Monorepo Builder to commit b68f56eff5f37884e655ba5d59331be67e251957
    https://github.com/symplify/symplify/commit/b68f56eff5f37884e655ba5d59331be67e251957 open 9.5-dev

--- src/Console/MonorepoBuilderConsoleApplication.php
+++ src/Console/MonorepoBuilderConsoleApplication.php
@@ -1,27 +1,23 @@
 <?php
 
-declare(strict_types=1);
+declare (strict_types=1);
+namespace MonorepoBuilder20210703\Symplify\MonorepoBuilder\Console;
 
-namespace Symplify\MonorepoBuilder\Console;
-
-use Symfony\Component\Console\Application;
-use Symfony\Component\Console\Command\Command;
-use Symplify\PackageBuilder\Console\Command\CommandNaming;
-
-final class MonorepoBuilderConsoleApplication extends Application
+use MonorepoBuilder20210703\Symfony\Component\Console\Application;
+use MonorepoBuilder20210703\Symfony\Component\Console\Command\Command;
+use MonorepoBuilder20210703\Symplify\PackageBuilder\Console\Command\CommandNaming;
+final class MonorepoBuilderConsoleApplication extends \MonorepoBuilder20210703\Symfony\Component\Console\Application
 {
     /**
      * @param Command[] $commands
      */
-    public function __construct(CommandNaming $commandNaming, array $commands)
+    public function __construct(\MonorepoBuilder20210703\Symplify\PackageBuilder\Console\Command\CommandNaming $commandNaming, array $commands)
     {
         foreach ($commands as $command) {
             $commandName = $commandNaming->resolveFromCommand($command);
             $command->setName($commandName);
-
             $this->add($command);
         }
-
         parent::__construct('Monorepo Builder');
     }
 }
