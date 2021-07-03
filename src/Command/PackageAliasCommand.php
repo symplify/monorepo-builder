0294efb94a51d45be7e74d45c3afc1d6c8073d2c
GitHub Action <action@github.com>
2021-07-03 10:42:49 +0000

    Monorepo Builder 9.4.7

--- src/Command/PackageAliasCommand.php
+++ src/Command/PackageAliasCommand.php
@@ -1,47 +1,51 @@
 <?php
 
-declare(strict_types=1);
+declare (strict_types=1);
+namespace MonorepoBuilder20210703\Symplify\MonorepoBuilder\Command;
 
-namespace Symplify\MonorepoBuilder\Command;
-
-use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Output\OutputInterface;
-use Symplify\MonorepoBuilder\DevMasterAliasUpdater;
-use Symplify\MonorepoBuilder\Finder\PackageComposerFinder;
-use Symplify\MonorepoBuilder\Git\ExpectedAliasResolver;
-use Symplify\PackageBuilder\Console\Command\AbstractSymplifyCommand;
-use Symplify\PackageBuilder\Console\ShellCode;
-
-final class PackageAliasCommand extends AbstractSymplifyCommand
+use MonorepoBuilder20210703\Symfony\Component\Console\Input\InputInterface;
+use MonorepoBuilder20210703\Symfony\Component\Console\Output\OutputInterface;
+use MonorepoBuilder20210703\Symplify\MonorepoBuilder\DevMasterAliasUpdater;
+use MonorepoBuilder20210703\Symplify\MonorepoBuilder\Finder\PackageComposerFinder;
+use MonorepoBuilder20210703\Symplify\MonorepoBuilder\Git\ExpectedAliasResolver;
+use MonorepoBuilder20210703\Symplify\PackageBuilder\Console\Command\AbstractSymplifyCommand;
+use MonorepoBuilder20210703\Symplify\PackageBuilder\Console\ShellCode;
+final class PackageAliasCommand extends \MonorepoBuilder20210703\Symplify\PackageBuilder\Console\Command\AbstractSymplifyCommand
 {
-    public function __construct(
-        private PackageComposerFinder $packageComposerFinder,
-        private DevMasterAliasUpdater $devMasterAliasUpdater,
-        private ExpectedAliasResolver $expectedAliasResolver
-    ) {
+    /**
+     * @var \Symplify\MonorepoBuilder\Finder\PackageComposerFinder
+     */
+    private $packageComposerFinder;
+    /**
+     * @var \Symplify\MonorepoBuilder\DevMasterAliasUpdater
+     */
+    private $devMasterAliasUpdater;
+    /**
+     * @var \Symplify\MonorepoBuilder\Git\ExpectedAliasResolver
+     */
+    private $expectedAliasResolver;
+    public function __construct(\MonorepoBuilder20210703\Symplify\MonorepoBuilder\Finder\PackageComposerFinder $packageComposerFinder, \MonorepoBuilder20210703\Symplify\MonorepoBuilder\DevMasterAliasUpdater $devMasterAliasUpdater, \MonorepoBuilder20210703\Symplify\MonorepoBuilder\Git\ExpectedAliasResolver $expectedAliasResolver)
+    {
+        $this->packageComposerFinder = $packageComposerFinder;
+        $this->devMasterAliasUpdater = $devMasterAliasUpdater;
+        $this->expectedAliasResolver = $expectedAliasResolver;
         parent::__construct();
     }
-
-    protected function configure(): void
+    protected function configure() : void
     {
         $this->setDescription('Updates branch alias in "composer.json" all found packages');
     }
-
-    protected function execute(InputInterface $input, OutputInterface $output): int
+    protected function execute(\MonorepoBuilder20210703\Symfony\Component\Console\Input\InputInterface $input, \MonorepoBuilder20210703\Symfony\Component\Console\Output\OutputInterface $output) : int
     {
         $composerPackageFiles = $this->packageComposerFinder->getPackageComposerFiles();
         if ($composerPackageFiles === []) {
             $this->symfonyStyle->error('No "composer.json" were found in packages.');
-            return ShellCode::ERROR;
+            return \MonorepoBuilder20210703\Symplify\PackageBuilder\Console\ShellCode::ERROR;
         }
-
         $expectedAlias = $this->expectedAliasResolver->resolve();
-
         $this->devMasterAliasUpdater->updateFileInfosWithAlias($composerPackageFiles, $expectedAlias);
-
-        $message = sprintf('Alias was updated to "%s" in all packages.', $expectedAlias);
+        $message = \sprintf('Alias was updated to "%s" in all packages.', $expectedAlias);
         $this->symfonyStyle->success($message);
-
-        return ShellCode::SUCCESS;
+        return \MonorepoBuilder20210703\Symplify\PackageBuilder\Console\ShellCode::SUCCESS;
     }
 }
