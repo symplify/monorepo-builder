e8f1bf02412ab06dc87a5cc71d04c5b314406961
GitHub <action@github.com>
2021-07-03 14:15:44 +0000

    open 9.5-dev

---
+++ packages/Merge/ComposerKeyMerger/PreferStableKeyMerger.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Symplify\MonorepoBuilder\Merge\ComposerKeyMerger;
+
+use Symplify\ComposerJsonManipulator\ValueObject\ComposerJson;
+use Symplify\MonorepoBuilder\Merge\Contract\ComposerKeyMergerInterface;
+
+final class PreferStableKeyMerger implements ComposerKeyMergerInterface
+{
+    public function merge(ComposerJson $mainComposerJson, ComposerJson $newComposerJson): void
+    {
+        if ($newComposerJson->getPreferStable() === null) {
+            return;
+        }
+
+        $mainComposerJson->setPreferStable($newComposerJson->getPreferStable());
+    }
+}
--- src/Git/MostRecentTagResolver.php
+++ src/Git/MostRecentTagResolver.php
@@ -1,46 +1,53 @@
 <?php
 
-declare (strict_types=1);
-namespace MonorepoBuilder20210703\Symplify\MonorepoBuilder\Git;
+declare(strict_types=1);
 
-use MonorepoBuilder20210703\Symplify\MonorepoBuilder\Release\Process\ProcessRunner;
+namespace Symplify\MonorepoBuilder\Git;
+
+use Symplify\MonorepoBuilder\Release\Process\ProcessRunner;
+
 final class MostRecentTagResolver
 {
-    /**
-     * @var \Symplify\MonorepoBuilder\Release\Process\ProcessRunner
-     */
-    private $processRunner;
-    public function __construct(\MonorepoBuilder20210703\Symplify\MonorepoBuilder\Release\Process\ProcessRunner $processRunner)
-    {
-        $this->processRunner = $processRunner;
+    public function __construct(
+        private ProcessRunner $processRunner
+    ) {
     }
+
     /**
      * Returns null, when there are no local tags yet
      */
-    public function resolve(string $gitDirectory) : ?string
+    public function resolve(string $gitDirectory): ?string
     {
         $command = ['git', 'tag', '-l', '--sort=committerdate'];
-        if (\getcwd() !== $gitDirectory) {
+
+        if (getcwd() !== $gitDirectory) {
             $command[] = '--git-dir';
             $command[] = $gitDirectory;
         }
+
         $tagList = $this->parseTags($this->processRunner->run($command));
+
         /** @var string $theMostRecentTag */
-        $theMostRecentTag = (string) \array_pop($tagList);
+        $theMostRecentTag = (string) array_pop($tagList);
+
         if ($theMostRecentTag === '') {
             return null;
         }
+
         return $theMostRecentTag;
     }
+
     /**
      * @return string[]
      */
-    private function parseTags(string $commandResult) : array
+    private function parseTags(string $commandResult): array
     {
-        $tags = \trim($commandResult);
+        $tags = trim($commandResult);
+
         // Remove all "\r" chars in case the CLI env like the Windows OS.
         // Otherwise (ConEmu, git bash, mingw cli, e.g.), leave as is.
-        $tags = \str_replace("\r", '', $tags);
-        return \explode("\n", $tags);
+        $tags = str_replace("\r", '', $tags);
+
+        return explode("\n", $tags);
     }
 }
