cb1e6ba3b550fa5db00b54aba4b0da4befafe4c8
GitHub Action <action@github.com>
2021-07-03 21:55:34 +0000

    Updated Monorepo Builder to commit b0d234047f6773ac9638c7e964c28f5fd924f2de
    https://github.com/symplify/symplify/commit/b0d234047f6773ac9638c7e964c28f5fd924f2de open 9.5-dev

--- src/Validator/ConflictingPackageVersionsReporter.php
+++ src/Validator/ConflictingPackageVersionsReporter.php
@@ -1,44 +1,41 @@
 <?php
 
-declare(strict_types=1);
+declare (strict_types=1);
+namespace MonorepoBuilder20210703\Symplify\MonorepoBuilder\Validator;
 
-namespace Symplify\MonorepoBuilder\Validator;
-
-use Symfony\Component\Console\Style\SymfonyStyle;
-use Symplify\SmartFileSystem\SmartFileInfo;
-
+use MonorepoBuilder20210703\Symfony\Component\Console\Style\SymfonyStyle;
+use MonorepoBuilder20210703\Symplify\SmartFileSystem\SmartFileInfo;
 final class ConflictingPackageVersionsReporter
 {
-    public function __construct(
-        private SymfonyStyle $symfonyStyle
-    ) {
+    /**
+     * @var \Symfony\Component\Console\Style\SymfonyStyle
+     */
+    private $symfonyStyle;
+    public function __construct(\MonorepoBuilder20210703\Symfony\Component\Console\Style\SymfonyStyle $symfonyStyle)
+    {
+        $this->symfonyStyle = $symfonyStyle;
     }
-
     /**
      * @param mixed[] $conflictingPackages
      */
-    public function report(array $conflictingPackages): void
+    public function report(array $conflictingPackages) : void
     {
         foreach ($conflictingPackages as $packageName => $filesToVersions) {
-            $message = sprintf('Package "%s" has incompatible version', $packageName);
+            $message = \sprintf('Package "%s" has incompatible version', $packageName);
             $this->symfonyStyle->title($message);
-
             $tableRows = $this->createTableRows($filesToVersions);
             $this->symfonyStyle->table(['File', 'Version'], $tableRows);
         }
-
         $this->symfonyStyle->error('Found conflicting package versions, fix them first.');
     }
-
     /**
      * @return array<int, mixed[]>
      */
-    private function createTableRows($filesToVersions): array
+    private function createTableRows($filesToVersions) : array
     {
         $tableRows = [];
-
         foreach ($filesToVersions as $file => $version) {
-            $fileInfo = new SmartFileInfo($file);
+            $fileInfo = new \MonorepoBuilder20210703\Symplify\SmartFileSystem\SmartFileInfo($file);
             $tableRows[] = [$fileInfo->getRelativeFilePathFromCwd(), $version];
         }
         return $tableRows;
