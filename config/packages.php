f9dcf4353cbd4142e47901c82b3c485ee6dd55f5
GitHub <action@github.com>
2021-07-03 14:17:55 +0000

    prepare release

--- config/packages.php
+++ config/packages.php
@@ -1,14 +1,20 @@
 <?php
 
-declare (strict_types=1);
-namespace MonorepoBuilder20210703;
+declare(strict_types=1);
 
-use MonorepoBuilder20210703\Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
-return static function (\MonorepoBuilder20210703\Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator $containerConfigurator) : void {
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+
+return static function (ContainerConfigurator $containerConfigurator): void {
     $services = $containerConfigurator->services();
-    $services->defaults()->public()->autowire()->autoconfigure();
-    $services->load('MonorepoBuilder20210703\Symplify\MonorepoBuilder\\', __DIR__ . '/../packages')->exclude([
-        // register manually
-        __DIR__ . '/../packages/Release/ReleaseWorker',
-    ]);
+
+    $services->defaults()
+        ->public()
+        ->autowire()
+        ->autoconfigure();
+
+    $services->load('Symplify\MonorepoBuilder\\', __DIR__ . '/../packages')
+        ->exclude([
+            // register manually
+            __DIR__ . '/../packages/Release/ReleaseWorker',
+        ]);
 };
