60338283330354e96385e4c4bad4e6c7c49b1480
GitHub Action <action@github.com>
2022-08-18 09:31:37 +0000

    Updated Monorepo Builder to commit c683610da599e5bb8a07eed012873d4d75f2f161
    https://github.com/symplify/symplify/commit/c683610da599e5bb8a07eed012873d4d75f2f161 [PHPStanRules] Refactor PreventDuplicateClassMethodRule to collector pattern (#4351)

--- vendor/autoload.php
+++ vendor/autoload.php
@@ -9,4 +9,4 @@ if (PHP_VERSION_ID < 50600) {
 
 require_once __DIR__ . '/composer/autoload_real.php';
 
-return ComposerAutoloaderInit974a3562fd0ec033803354155ea6a8a8::getLoader();
+return ComposerAutoloaderInit7268f53579f68bbd5fcd25ebe0652e46::getLoader();
--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -4,7 +4,7 @@
 
 namespace Composer\Autoload;
 
-class ComposerStaticInit974a3562fd0ec033803354155ea6a8a8
+class ComposerStaticInit7268f53579f68bbd5fcd25ebe0652e46
 {
     public static $files = array (
         '6e3fae29631ef280660b3cdad06f25a8' => __DIR__ . '/..' . '/symfony/deprecation-contracts/function.php',
@@ -803,9 +803,9 @@ class ComposerStaticInit974a3562fd0ec033803354155ea6a8a8
     public static function getInitializer(ClassLoader $loader)
     {
         return \Closure::bind(function () use ($loader) {
-            $loader->prefixLengthsPsr4 = ComposerStaticInit974a3562fd0ec033803354155ea6a8a8::$prefixLengthsPsr4;
-            $loader->prefixDirsPsr4 = ComposerStaticInit974a3562fd0ec033803354155ea6a8a8::$prefixDirsPsr4;
-            $loader->classMap = ComposerStaticInit974a3562fd0ec033803354155ea6a8a8::$classMap;
+            $loader->prefixLengthsPsr4 = ComposerStaticInit7268f53579f68bbd5fcd25ebe0652e46::$prefixLengthsPsr4;
+            $loader->prefixDirsPsr4 = ComposerStaticInit7268f53579f68bbd5fcd25ebe0652e46::$prefixDirsPsr4;
+            $loader->classMap = ComposerStaticInit7268f53579f68bbd5fcd25ebe0652e46::$classMap;
 
         }, null, ClassLoader::class);
     }
