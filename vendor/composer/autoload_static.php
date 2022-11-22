289920ab374379e00d0bb72a6a33302f639eab32
GitHub Action <action@github.com>
2022-11-22 21:03:25 +0000

    Updated Monorepo Builder to commit 11ae2848791c45cedb605aa1366df2b70f4e38b4
    https://github.com/symplify/symplify/commit/11ae2848791c45cedb605aa1366df2b70f4e38b4 Allow sebastian/diff 5 (#4478)

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -4,7 +4,7 @@
 
 namespace Composer\Autoload;
 
-class ComposerStaticInit0ca43d883659b4aa208336f4b08aae81
+class ComposerStaticInit6519c9be418d5708b4ea2c7c41dc21a7
 {
     public static $files = array (
         '6e3fae29631ef280660b3cdad06f25a8' => __DIR__ . '/..' . '/symfony/deprecation-contracts/function.php',
@@ -793,9 +793,9 @@ class ComposerStaticInit0ca43d883659b4aa208336f4b08aae81
     public static function getInitializer(ClassLoader $loader)
     {
         return \Closure::bind(function () use ($loader) {
-            $loader->prefixLengthsPsr4 = ComposerStaticInit0ca43d883659b4aa208336f4b08aae81::$prefixLengthsPsr4;
-            $loader->prefixDirsPsr4 = ComposerStaticInit0ca43d883659b4aa208336f4b08aae81::$prefixDirsPsr4;
-            $loader->classMap = ComposerStaticInit0ca43d883659b4aa208336f4b08aae81::$classMap;
+            $loader->prefixLengthsPsr4 = ComposerStaticInit6519c9be418d5708b4ea2c7c41dc21a7::$prefixLengthsPsr4;
+            $loader->prefixDirsPsr4 = ComposerStaticInit6519c9be418d5708b4ea2c7c41dc21a7::$prefixDirsPsr4;
+            $loader->classMap = ComposerStaticInit6519c9be418d5708b4ea2c7c41dc21a7::$classMap;
 
         }, null, ClassLoader::class);
     }
