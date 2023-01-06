ede50d9c9323ad28e013adba474e5259392c8923
Abdul Malik Ikhsan <samsonasik@gmail.com>
2023-01-06 08:51:10 +0700

    add packages directory to prefix code

--- prefix-code.sh
+++ prefix-code.sh
@@ -31,7 +31,7 @@ wget https://github.com/humbug/php-scoper/releases/download/0.17.5/php-scoper.ph
 note "Running php-scoper"
 
 # Work around possible PHP memory limits
-php -d memory_limit=-1 php-scoper.phar add-prefix bin config src vendor composer.json --config scoper.php --force --ansi --output-dir scoped-code
+php -d memory_limit=-1 php-scoper.phar add-prefix bin config src packages vendor composer.json --config scoper.php --force --ansi --output-dir scoped-code
 
 # the output code is in "/scoped-code", lets move it up
 # the local directories have to be empty to move easily
