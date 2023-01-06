12c2683c94bfa87a6ac5e35694fe96a1f0fd0415
Abdul Malik Ikhsan <samsonasik@gmail.com>
2023-01-06 08:57:39 +0700

    add packages directory to prefix code

--- prefix-code.sh
+++ prefix-code.sh
@@ -35,7 +35,7 @@ php -d memory_limit=-1 php-scoper.phar add-prefix bin config src packages vendor
 
 # the output code is in "/scoped-code", lets move it up
 # the local directories have to be empty to move easily
-rm -r bin config src vendor composer.json
+rm -r bin config src vendor composer.json packages
 mv scoped-code/* .
 
 note "Dumping Composer Autoload"
