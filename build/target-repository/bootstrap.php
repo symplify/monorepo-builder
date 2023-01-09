e4f85642f613a410caa6f48179cad41831ec532e
Abdul Malik Ikhsan <samsonasik@gmail.com>
2023-01-10 00:50:36 +0700

    add boostrasp.php

---
+++ build/target-repository/bootstrap.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types = 1);
+
+// inspired by https://github.com/phpstan/phpstan/blob/master/bootstrap.php
+spl_autoload_register(function (string $class): void {
+    static $composerAutoloader;
+
+    // already loaded in bin/monorepo-builder.php
+    if (defined('__MONOREPO_BUILDER_RUNNING__')) {
+        return;
+    }
+
+    // load prefixed or native class, e.g. for running tests
+    if (strpos($class, 'MonorepoBuilder\\') === 0) {
+        if ($composerAutoloader === null) {
+            // prefixed version autoload
+            $composerAutoloader = require __DIR__ . '/vendor/autoload.php';
+        }
+
+        // some weird collision with PHPStan custom rule tests
+        if (! is_int($composerAutoloader)) {
+            $composerAutoloader->loadClass($class);
+        }
+    }
+});
