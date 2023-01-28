b813b28b11a17e7b82844bc26990871f824accf8
GitHub Action <action@github.com>
2023-01-28 10:26:56 +0000

    release PHP 7.2 downgraded

--- packages/ComposerJsonManipulator/Sorter/ComposerPackageSorter.php
+++ packages/ComposerJsonManipulator/Sorter/ComposerPackageSorter.php
@@ -1,11 +1,9 @@
 <?php
 
-declare(strict_types=1);
-
+declare (strict_types=1);
 namespace Symplify\MonorepoBuilder\ComposerJsonManipulator\Sorter;
 
-use Nette\Utils\Strings;
-
+use MonorepoBuilderPrefix202301\Nette\Utils\Strings;
 /**
  * Mostly inspired by https://github.com/composer/composer/blob/master/src/Composer/Json/JsonManipulator.php
  *
@@ -18,13 +16,11 @@ final class ComposerPackageSorter
      * @var string
      */
     private const PLATFORM_PACKAGE_REGEX = '#^(?:php(?:-64bit|-ipv6|-zts|-debug)?|hhvm|(?:ext|lib)-[a-z0-9](?:[_.-]?[a-z0-9]+)*|composer-(?:plugin|runtime)-api)$#iD';
-
     /**
      * @see https://regex101.com/r/SXZcfb/1
      * @var string
      */
-    private const REQUIREMENT_TYPE_REGEX = '#^(?<name>php|hhvm|ext|lib|\D)#';
-
+    private const REQUIREMENT_TYPE_REGEX = '#^(?<name>php|hhvm|ext|lib|\\D)#';
     /**
      * Sorts packages by importance (platform packages first, then PHP dependencies) and alphabetically.
      *
@@ -33,51 +29,36 @@ final class ComposerPackageSorter
      * @param array<string, string> $packages
      * @return array<string, string>
      */
-    public function sortPackages(array $packages): array
+    public function sortPackages(array $packages) : array
     {
-        uksort(
-            $packages,
-            fn (string $firstPackageName, string $secondPackageName): int => $this->createNameWithPriority(
-                $firstPackageName
-            ) <=> $this->createNameWithPriority($secondPackageName)
-        );
-
+        \uksort($packages, function (string $firstPackageName, string $secondPackageName) : int {
+            return $this->createNameWithPriority($firstPackageName) <=> $this->createNameWithPriority($secondPackageName);
+        });
         return $packages;
     }
-
-    private function createNameWithPriority(string $requirementName): string
+    private function createNameWithPriority(string $requirementName) : string
     {
         if ($this->isPlatformPackage($requirementName)) {
-            return Strings::replace(
-                $requirementName,
-                self::REQUIREMENT_TYPE_REGEX,
-                static function (array $match): string {
-                    $name = $match['name'];
-                    if ($name === 'php') {
-                        return '0-' . $name;
-                    }
-
-                    if ($name === 'hhvm') {
-                        return '0-' . $name;
-                    }
-
-                    if ($name === 'ext') {
-                        return '1-' . $name;
-                    }
-
-                    if ($name === 'lib') {
-                        return '2-' . $name;
-                    }
-
-                    return '3-' . $name;
+            return Strings::replace($requirementName, self::REQUIREMENT_TYPE_REGEX, static function (array $match) : string {
+                $name = $match['name'];
+                if ($name === 'php') {
+                    return '0-' . $name;
                 }
-            );
+                if ($name === 'hhvm') {
+                    return '0-' . $name;
+                }
+                if ($name === 'ext') {
+                    return '1-' . $name;
+                }
+                if ($name === 'lib') {
+                    return '2-' . $name;
+                }
+                return '3-' . $name;
+            });
         }
-
         return '4-' . $requirementName;
     }
-
-    private function isPlatformPackage(string $name): bool
+    private function isPlatformPackage(string $name) : bool
     {
         return (bool) Strings::match($name, self::PLATFORM_PACKAGE_REGEX);
     }
