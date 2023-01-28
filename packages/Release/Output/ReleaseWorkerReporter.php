680ce13f48fa9e213db934b89fecd841cd031aaa
GitHub Action <action@github.com>
2023-01-28 15:09:50 +0000

    release PHP 7.2 downgraded

--- packages/Release/Output/ReleaseWorkerReporter.php
+++ packages/Release/Output/ReleaseWorkerReporter.php
@@ -1,32 +1,31 @@
 <?php
 
-declare(strict_types=1);
-
+declare (strict_types=1);
 namespace Symplify\MonorepoBuilder\Release\Output;
 
-use Symfony\Component\Console\Style\SymfonyStyle;
+use MonorepoBuilderPrefix202301\Symfony\Component\Console\Style\SymfonyStyle;
 use Symplify\MonorepoBuilder\Release\Contract\ReleaseWorker\ReleaseWorkerInterface;
 use Symplify\MonorepoBuilder\Release\Contract\ReleaseWorker\StageAwareInterface;
-
 final class ReleaseWorkerReporter
 {
-    public function __construct(
-        private SymfonyStyle $symfonyStyle
-    ) {
-    }
-
-    public function printMetadata(ReleaseWorkerInterface $releaseWorker): void
+    /**
+     * @var \Symfony\Component\Console\Style\SymfonyStyle
+     */
+    private $symfonyStyle;
+    public function __construct(SymfonyStyle $symfonyStyle)
     {
-        if (! $this->symfonyStyle->isVerbose()) {
+        $this->symfonyStyle = $symfonyStyle;
+    }
+    public function printMetadata(ReleaseWorkerInterface $releaseWorker) : void
+    {
+        if (!$this->symfonyStyle->isVerbose()) {
             return;
         }
-
         // show debug data on -v/--verbose/--debug
-        $this->symfonyStyle->writeln('class: ' . $releaseWorker::class);
+        $this->symfonyStyle->writeln('class: ' . \get_class($releaseWorker));
         if ($releaseWorker instanceof StageAwareInterface) {
             $this->symfonyStyle->writeln('stage: ' . $releaseWorker->getStage());
         }
-
         $this->symfonyStyle->newLine();
     }
 }
