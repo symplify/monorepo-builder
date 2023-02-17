b3ed3000d7e76080396d36171707e07baf08b201
Anton Evers <anton@eve.rs>
2023-02-17 15:33:44 +0100

    Bump interdependencies bumped too much
    This method upgrades interdependencies for packages that have the same vendor prefix as the root project.
    But it also upgrades interdependencies for packages that have a vendor prefix that starts with your vendor prefix.
    
    In other words, if your vendor prefix is "ex" and your packages are called "ex/package-test", then it would also bump "example/package-other".
    This is because it matches "starts with ex".
    Instead it should start with "ex/" to match the full vendor name plus the separator.
    That way it does not include other vendors that start with the same prefix as you as part of their name.

--- src/Command/BumpInterdependencyCommand.php
+++ src/Command/BumpInterdependencyCommand.php
@@ -57,7 +57,7 @@ final class BumpInterdependencyCommand extends AbstractSymplifyCommand
 
         $this->dependencyUpdater->updateFileInfosWithVendorAndVersion(
             $this->composerJsonProvider->getPackagesComposerFileInfos(),
-            $vendorName,
+            $vendorName . '/',
             $version
         );
 
