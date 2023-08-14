e35a80486de52d9009dd808e3b6edd491c270b7c
Kay Wei <weikaiii@sina.cn>
2023-08-14 15:53:38 +0800

    When executing the release, if the current branch is not the same as the default branch, user confirmation is required before proceeding

--- packages/Release/ReleaseWorker/TagVersionReleaseWorker.php
+++ packages/Release/ReleaseWorker/TagVersionReleaseWorker.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Symplify\MonorepoBuilder\Release\ReleaseWorker;
 
 use PharIo\Version\Version;
+use Symfony\Component\Console\Style\SymfonyStyle;
 use Symplify\MonorepoBuilder\Release\Contract\ReleaseWorker\ReleaseWorkerInterface;
 use Symplify\MonorepoBuilder\Release\Process\ProcessRunner;
 use Symplify\MonorepoBuilder\ValueObject\Option;
@@ -20,6 +21,17 @@ final class TagVersionReleaseWorker implements ReleaseWorkerInterface
         ParameterProvider $parameterProvider
     ) {
         $this->branchName = $parameterProvider->provideStringParameter(Option::DEFAULT_BRANCH_NAME);
+    }
+
+    /**
+     * @return array<string,callable(): bool|string>
+     */
+    public function shouldConfirm(): array
+    {
+        return [
+            'whenTrue' => fn(): bool => self::getCurrentBranch() !== self::getDefaultBranch(),
+            'message'=> sprintf('Do you want to release it on the [ %s ] branch?',self::getCurrentBranch())
+        ];
     }
 
     public function work(Version $version): void
@@ -41,5 +53,19 @@ final class TagVersionReleaseWorker implements ReleaseWorkerInterface
     public function getDescription(Version $version): string
     {
         return sprintf('Add local tag "%s"', $version->getOriginalString());
+    }
+
+    private function getCurrentBranch(): string
+    {
+        exec('git rev-parse --abbrev-ref HEAD',$outputs);
+
+        return $outputs[0];
+    }
+
+    private function getDefaultBranch(): string
+    {
+        exec("git symbolic-ref refs/remotes/origin/HEAD | sed 's@^refs/remotes/origin/@@'",$outputs);
+
+        return $outputs[0];
     }
 }
