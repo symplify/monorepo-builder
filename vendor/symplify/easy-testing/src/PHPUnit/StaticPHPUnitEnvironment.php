bde0c97054fc90b53bf5759c7b1f5e1c4a959c4b
GitHub Action <action@github.com>
2023-08-16 08:35:39 +0000

    release PHP 7.2 downgraded

---
+++ vendor/symplify/easy-testing/src/PHPUnit/StaticPHPUnitEnvironment.php
@@ -0,0 +1,18 @@
+<?php
+
+declare (strict_types=1);
+namespace MonorepoBuilderPrefix202308\Symplify\EasyTesting\PHPUnit;
+
+/**
+ * @api
+ */
+final class StaticPHPUnitEnvironment
+{
+    /**
+     * Never ever used static methods if not neccesary, this is just handy for tests + src to prevent duplication.
+     */
+    public static function isPHPUnitRun() : bool
+    {
+        return \defined('MonorepoBuilderPrefix202308\\PHPUNIT_COMPOSER_INSTALL') || \defined('MonorepoBuilderPrefix202308\\__PHPUNIT_PHAR__');
+    }
+}
