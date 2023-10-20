a8f1ca07268a266da313c167613934ac7cb72dc2
Kay W <weikaiii@sina.cn>
2023-10-20 01:26:39 -0500

    Revert Rector version to 0.17 (#64)

--- full-tool-build.sh
+++ full-tool-build.sh
@@ -11,7 +11,7 @@ rm -rf tests
 
 # downgrade with rector
 mkdir rector-local
-composer require rector/rector --working-dir rector-local
+composer require rector/rector ^0.17 --working-dir rector-local
 rector-local/vendor/bin/rector process bin src packages vendor --config build/rector-downgrade-php-72.php --ansi
 
 # prefix
